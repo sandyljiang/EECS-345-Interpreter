6a5f8fac4c2a75e8d5170b1d3c94b59afda4fd2b
itaintgoodwithoutmsg <srn24@case.edu>
2019-04-28 13:13:52 -0400

    added code that causes infinite loop. Attempting different solution by handling 'super' like 'this'

--- interpreter/mstate-mvalue.rkt
+++ interpreter/mstate-mvalue.rkt
@@ -801,7 +801,7 @@
     (if (list? (mvalue-func-call-name expr)) ; If the function call is a dot operator
         (let* ((LHS (get-dot-LHS (dot-lhs (mvalue-func-call-name expr)) env class-closure instance throw))
                (LHS-func-lookup (if (eq? (dot-lhs (mvalue-func-call-name expr)) 'super)
-                                    (super (get-class-closure LHS))
+                                    (find (super (get-class-closure LHS)) env)
                                     (get-class-closure LHS)))
                (RHS (lookup-function-closure (dot-rhs (mvalue-func-call-name expr)) empty-env LHS-func-lookup)))
               (newline) (display "RHS: ") (display RHS) (newline) (mstate-function-call expr env ((closure-class RHS) env) instance LHS RHS throw))
